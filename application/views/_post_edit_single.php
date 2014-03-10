ac476849ed48ad4074e66389492cc4aa276b3c87
Shane-S <shane.d.spoor@gmail.com>
2014-03-09 23:27:53 -0700

    Image upload working
    The image upload functionality currently works. Images are correcty linked with posts, their thumbnails load
    correctly, and they can be uploaded as part of post creation or editing.

--- application/views/_post_edit_single.php
+++ application/views/_post_edit_single.php
@@ -1,5 +1,6 @@
-<form class="forms forms-columnar" action="/postmtce/submit/{postid}" method="post">
+<form class="forms forms-columnar" action="/postmtce/submit/{postid}" method="post" enctype="multipart/form-data">
     {post_title}
+    {image_upload}
     {slug}
     {date_picker}
     {text_editor}
